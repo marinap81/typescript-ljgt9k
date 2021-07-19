f1f42ef4ea1d42e76d5cbacdef837ad80642ecf3
marinap81 <marinapajvancic@hotmail.com>
2021-07-19 12:51:51 +1000

    2 files modified

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-// Import stylesheets
+//Import stylesheets
 //import './style.css';
 
 // Write TypeScript code!
@@ -7,40 +7,14 @@ appDiv.innerHTML = `<h1>TypeScript , HELLO </h1>`;
 
 //typescript is a javascript generator//
 
-const planet: string = "Earth" //declaring variable with name
-const isLockdown: boolean = true
-const year: number = 2021
-
-//var animal = "dog"; //if datatype not initialised typescript will read first word as the default datatype
-//animal = 5; //error would occur because this is an int
-
-//let food: any = "noodle" //recoommend not using, better to enforce type of variable//
-//food = 3;
-
-//planet = 3; //only shows error in complier but not error when run-this is because typscrip gets translated
-//javascript.
-//console.log(planet);
-//console.log(year);
-
-//let and constant//
-//const = declare varible once, cant change variable afterwards
-
-//const colour = "green";
-colour = "red";
-
-
-let year: number = 2020;
-console.log(year);
-year = 2020
-
-const button: HTMLElement = document.getElementById('custom-btn')
-button.addEventListener('click', event =>) {
-  console.log('clicking');
-});
-
+const output: HTMLElement = document.getElementById('output');
+const button: HTMLElement = document.getElementById('custom-btn');
+button.addEventListener('click', fizzbuzz);
 
 function fizzbuzz(){
 
+  const startNumInput: HTMLInputElement = <HTMLInputElement>
+  (document.getElementById('start-num')
   //count = 100
 
   for (let i = 1; i < 100; i++)
@@ -48,6 +22,7 @@ function fizzbuzz(){
     let _i = i + 1;
 
     if  (_i % 3 == 0 && _i % 5==0) //% modulus-Returns the division remainder, same as i divided by 3 but give me just the numbers after the decimal,
+                                  //this example is asking for 0 remainder- ie you need to ask yourself how many times can 5 fit into 100
     {
       console.log('fizz', _i);
     }
